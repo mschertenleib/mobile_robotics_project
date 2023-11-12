093089829bfcf336c2a5a25e12e284c006f4114e
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2023-11-12 20:31:56 +0100

    Implement bitmap-based intersection check

--- geometry.py
+++ geometry.py
@@ -32,7 +32,7 @@ def intersect_segments(a1: np.ndarray, a2: np.ndarray, b1: np.ndarray, b2: np.nd
         (((cb1 > 0) & (cb2 < 0)) | ((cb1 < 0) & (cb2 > 0))) & (((ca1 > 0) & (ca2 < 0)) | ((ca1 < 0) & (ca2 > 0))))
 
 
-def segment_intersects_contours(pt1, pt2, contours):
+def intersect_contours(pt1: np.ndarray, pt2: np.ndarray, contours) -> bool:
     # NOTE: it is very important here that segment_intersects_segment() returns False when two segments only
     # touch but do not cross. If that was not the case (ie. if it returned True), we would have to consider
     # the special case when intersecting an end point with the contour it comes from. Also, due to the kind
@@ -42,15 +42,17 @@ def segment_intersects_contours(pt1, pt2, contours):
     # contour while always staying inside; it would always cross the contour at some point, and hence be
     # detected as intersecting.
 
-    # FIXME: some intersections are not detected (it would seem only intra-contour) when using a small epsilon
     for contour in contours:
         if intersect_segments(pt1, pt2, contour[:-1, 0], contour[1:, 0]):
+            return True
+        if intersect_segments(pt1, pt2, contour[-1, 0], contour[0, 0]):
             return True
     return False
 
 
-def extract_static_edges(contours):
+def extract_static_edges(contours, obstacle_mask):
     edges = []
+    line_img = np.zeros(obstacle_mask.shape, dtype=np.uint8)
     for ci in range(len(contours)):
         contour_i = np.squeeze(contours[ci])
         for i in range(len(contour_i)):
@@ -77,10 +79,10 @@ def extract_static_edges(contours):
 
                     t = contour_j[j] - contour_i[i]
                     n = (-t[1], t[0])
-                    ndui = np.dot(n, ui)
-                    ndvi = np.dot(n, vi)
-                    nduj = np.dot(n, uj)
-                    ndvj = np.dot(n, vj)
+                    ndui = np.dot(n, ui)  # = np.cross(t, ui)
+                    ndvi = np.dot(n, vi)  # = np.cross(t, vi)
+                    nduj = np.dot(n, uj)  # = np.cross(t, uj)
+                    ndvj = np.dot(n, vj)  # = np.cross(t, vj)
 
                     # Only keep vertices for which the previous and next one both lie on the same
                     # side of the line, or on the line itself.
@@ -89,16 +91,60 @@ def extract_static_edges(contours):
                     if (nduj < 0 or ndvj < 0) and (nduj > 0 or ndvj > 0):
                         continue
 
-                    # Discard edges that intersect contours
-                    if segment_intersects_contours(contour_i[i], contour_j[j], contours):
-                        continue
+                    if True:  # Use bitmap intersection check
+                        cv2.line(line_img, contour_i[i], contour_j[j], color=[1])
+                        if np.any(line_img & obstacle_mask):
+                            line_img = np.zeros(obstacle_mask.shape, dtype=np.uint8)
+                            continue
+                    else:
+
+                        # The following handles this special case, where the intersection would otherwise not be caught:
+                        #            ______
+                        #           /######\
+                        #    j_____/########\_____i
+                        #   /######################\
+                        #  /########################\
+                        #
+                        # Note that this can only happen between vertices of the same contour due to the checks we do
+                        # before
+                        if ci == cj:
+                            if ndui == 0 and np.dot(t, ui) > 0:
+                                pim0 = contour_i[i]
+                                pim1 = contour_i[i - 1 if i > 0 else len(contour_i) - 1]
+                                pim2 = contour_i[i - 2 if i > 1 else len(contour_i) - 1]
+                                if np.cross(pim2 - pim1, pim0 - pim1) < 0:
+                                    continue
+                            if ndvi == 0 and np.dot(t, vi) > 0:
+                                pip0 = contour_i[i]
+                                pip1 = contour_i[i + 1 if i < len(contour_i) - 1 else 0]
+                                pip2 = contour_i[i + 2 if i < len(contour_i) - 2 else 0]
+                                if np.cross(pip0 - pip1, pip2 - pip1) < 0:
+                                    continue
+                            if nduj == 0 and np.dot(t, uj) < 0:
+                                pjm0 = contour_j[j]
+                                pjm1 = contour_j[j - 1 if j > 0 else len(contour_j) - 1]
+                                pjm2 = contour_j[j - 2 if j > 1 else len(contour_j) - 1]
+                                if np.cross(pjm2 - pjm1, pjm0 - pjm1) < 0:
+                                    continue
+                            if ndvj == 0 and np.dot(t, vj) < 0:
+                                pjp0 = contour_j[j]
+                                pjp1 = contour_j[j + 1 if j < len(contour_j) - 1 else 0]
+                                pjp2 = contour_j[j + 2 if j < len(contour_j) - 2 else 0]
+                                if np.cross(pjp0 - pjp1, pjp2 - pjp1) < 0:
+                                    continue
+
+                        # Discard edges that intersect contours
+                        if intersect_contours(contour_i[i], contour_j[j], contours):
+                            continue
 
                     edges.append([contour_i[i].tolist(), contour_j[j].tolist()])
 
     return edges
 
 
-def extract_dynamic_edges(contours, point):
+def extract_dynamic_edges(contours, obstacle_mask, point):
+    # FIXME: We should try to avoid duplication anyway.
+    line_img = np.zeros(obstacle_mask.shape, dtype=np.uint8)
     edges = []
     for cj in range(len(contours)):
         contour_j = np.squeeze(contours[cj])
@@ -117,7 +163,9 @@ def extract_dynamic_edges(contours, point):
             if (nduj < 0 or ndvj < 0) and (nduj > 0 or ndvj > 0):
                 continue
 
-            if segment_intersects_contours(point, contour_j[j], contours):
+            cv2.line(line_img, point, contour_j[j], color=[1])
+            if np.any(line_img & obstacle_mask):
+                line_img = np.zeros(obstacle_mask.shape, dtype=np.uint8)
                 continue
 
             edges.append([point.tolist(), contour_j[j].tolist()])
@@ -147,7 +195,6 @@ def main():
     approx_poly_epsilon = 2
     start_point = (200, 100)
     target_point = (120, 730)
-
     original_img = cv2.imread('map.png')
     img = cv2.cvtColor(original_img, cv2.COLOR_BGR2GRAY)
     _, img = cv2.threshold(img, threshold, 1, cv2.THRESH_BINARY_INV)
@@ -172,7 +219,7 @@ def main():
     # which is actually very likely
     contours, hierarchy = cv2.findContours(img, mode=cv2.RETR_LIST,
                                            method=cv2.CHAIN_APPROX_SIMPLE)
-    contours = [cv2.approxPolyDP(contour, approx_poly_epsilon, True) for contour in contours]
+    contours = [cv2.approxPolyDP(contour, epsilon=approx_poly_epsilon, closed=True) for contour in contours]
 
     # NOTE: orientation is positive for a clockwise contour, which is the opposite of the mathematical standard
     # (right-hand rule). Note however that the outer contour of a shape is always
@@ -184,16 +231,26 @@ def main():
     # robot is located, we can use them directly for computing the visibility
     # graph. Else, we must take into account the hierarchy.
 
-    edges = extract_static_edges(contours)
-    print(f'Number of static edges: {len(edges)}')
-    start_edges = extract_dynamic_edges(contours, np.array(start_point))
-    target_edges = extract_dynamic_edges(contours, np.array(target_point))
-
     walkable = np.zeros(original_img.shape, dtype=np.uint8)
     walkable[:] = (192, 64, 64)
     cv2.drawContours(walkable, contours, contourIdx=-1, color=(255, 255, 255), thickness=-1)
     img = cv2.addWeighted(original_img, 0.75, walkable, 0.25, 0.0)
     cv2.drawContours(img, contours, contourIdx=-1, color=(192, 64, 64))
+
+    obstacle_mask = np.zeros(original_img.shape[0:2], dtype=np.uint8)
+    cv2.drawContours(obstacle_mask, contours, contourIdx=-1, color=[1])
+    mask = np.zeros((obstacle_mask.shape[0] + 2, obstacle_mask.shape[1] + 2), dtype=np.uint8)
+    _, obstacle_mask, _, _ = cv2.floodFill(obstacle_mask, mask=mask, seedPoint=start_point, newVal=[1])
+    assert np.min(obstacle_mask) == 0
+    assert np.max(obstacle_mask) == 1
+    obstacle_mask = 1 - obstacle_mask
+
+    edges = extract_static_edges(contours, obstacle_mask)
+    print(f'Number of static edges: {len(edges)}')
+    start_edges = extract_dynamic_edges(contours, obstacle_mask, np.array(start_point))
+    target_edges = extract_dynamic_edges(contours, obstacle_mask, np.array(target_point))
+    print(f'Number of dynamic edges: {len(start_edges) + len(target_edges)}')
+
     for edge in edges:
         cv2.line(img, edge[0], edge[1], color=(0, 0, 0))
     for edge in start_edges:
