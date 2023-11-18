7bb8241f741fe6b5a9e2a19c4ace8f85003c00cd
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2023-11-18 17:40:33 +0100

    Update intersection benchmark

--- tests/benchmark_intersection.py
+++ tests/benchmark_intersection.py
@@ -3,28 +3,52 @@ import timeit
 import numpy as np
 
 
-def intersect_segments_cross(a1: np.ndarray, a2: np.ndarray, b1: np.ndarray, b2: np.ndarray) -> bool:
-    delta_a = a2 - a1
-    delta_b = b2 - b1
-    c_b1a2_b1b2 = np.cross(a2 - b1, delta_b)
-    c_b1a1_b1b2 = np.cross(a1 - b1, delta_b)
-    c_a1b2_a1a2 = np.cross(b2 - a1, delta_a)
-    c_a1b1_a1a2 = np.cross(b1 - a1, delta_a)
-    return np.any(
-        (((c_b1a2_b1b2 >= 0) & (c_b1a1_b1b2 <= 0)) | ((c_b1a2_b1b2 <= 0) & (c_b1a1_b1b2 >= 0))) & (
-                ((c_a1b2_a1a2 >= 0) & (c_a1b1_a1a2 <= 0)) | ((c_a1b2_a1a2 <= 0) & (c_a1b1_a1a2 >= 0))))
+def segments_intersect_cross(a: np.ndarray, b: np.ndarray, c: np.ndarray, d: np.ndarray) -> bool:
+    ab = b - a
+    cd = d - c
+    ac = c - a
+    cb_cd = np.cross(b - c, cd)
+    ca_cd = np.cross(a - c, cd)
+    ad_ab = np.cross(d - a, ab)
+    ac_ab = np.cross(ac, ab)
+
+    # Detects intersections between non-colinear segments
+    if np.any(
+            (((cb_cd >= 0) & (ca_cd < 0)) | ((cb_cd <= 0) & (ca_cd > 0))) & (
+                    ((ad_ab > 0) & (ac_ab < 0)) | ((ad_ab < 0) & (ac_ab > 0)))):
+        return True
+
+    colinear_mask = (cb_cd == 0) & (ca_cd == 0) & (ad_ab == 0) & (ac_ab == 0)
+    ac_dot_ab = ac[:, 0] * ab[0] + ac[:, 1] * ab[1]
+    ab_dot_cd = ab[0] * cd[:, 0] + ab[1] * cd[:, 1]
+    ab_dot_ab = ab[0] * ab[0] + ab[1] * ab[1]
+    mask_dir_same = (ab_dot_cd > 0) & (ac_dot_ab + ab_dot_cd > 0) & (ac_dot_ab - ab_dot_ab <= 0)
+    mask_dir_opposite = (ab_dot_cd < 0) & (ac_dot_ab > 0) & (ac_dot_ab + ab_dot_cd - ab_dot_ab <= 0)
+    return np.any(colinear_mask & (mask_dir_same | mask_dir_opposite))
 
 
-def intersect_segments_explicit(a1: np.ndarray, a2: np.ndarray, b1: np.ndarray, b2: np.ndarray) -> bool:
-    delta_a = a2 - a1
-    delta_b = b2 - b1
-    c_b1a2_b1b2 = (a2[0] - b1[:, 0]) * delta_b[:, 1] - (a2[1] - b1[:, 1]) * delta_b[:, 0]
-    c_b1a1_b1b2 = (a1[0] - b1[:, 0]) * delta_b[:, 1] - (a1[1] - b1[:, 1]) * delta_b[:, 0]
-    c_a1b2_a1a2 = (b2[:, 0] - a1[0]) * delta_a[1] - (b2[:, 1] - a1[1]) * delta_a[0]
-    c_a1b1_a1a2 = (b1[:, 0] - a1[0]) * delta_a[1] - (b1[:, 1] - a1[1]) * delta_a[0]
-    return np.any(
-        (((c_b1a2_b1b2 >= 0) & (c_b1a1_b1b2 <= 0)) | ((c_b1a2_b1b2 <= 0) & (c_b1a1_b1b2 >= 0))) & (
-                ((c_a1b2_a1a2 >= 0) & (c_a1b1_a1a2 <= 0)) | ((c_a1b2_a1a2 <= 0) & (c_a1b1_a1a2 >= 0))))
+def segments_intersect_explicit(a: np.ndarray, b: np.ndarray, c: np.ndarray, d: np.ndarray) -> bool:
+    ab = b - a
+    cd = d - c
+    ac = c - a
+    cb_cd = (b[0] - c[:, 0]) * cd[:, 1] - (b[1] - c[:, 1]) * cd[:, 0]
+    ca_cd = (a[0] - c[:, 0]) * cd[:, 1] - (a[1] - c[:, 1]) * cd[:, 0]
+    ad_ab = (d[:, 0] - a[0]) * ab[1] - (d[:, 1] - a[1]) * ab[0]
+    ac_ab = ac[:, 0] * ab[1] - ac[:, 1] * ab[0]
+
+    # Detects intersections between non-colinear segments
+    if np.any(
+            (((cb_cd >= 0) & (ca_cd < 0)) | ((cb_cd <= 0) & (ca_cd > 0))) & (
+                    ((ad_ab > 0) & (ac_ab < 0)) | ((ad_ab < 0) & (ac_ab > 0)))):
+        return True
+
+    colinear_mask = (cb_cd == 0) & (ca_cd == 0) & (ad_ab == 0) & (ac_ab == 0)
+    ac_dot_ab = ac[:, 0] * ab[0] + ac[:, 1] * ab[1]
+    ab_dot_cd = ab[0] * cd[:, 0] + ab[1] * cd[:, 1]
+    ab_dot_ab = ab[0] * ab[0] + ab[1] * ab[1]
+    mask_dir_same = (ab_dot_cd > 0) & (ac_dot_ab + ab_dot_cd > 0) & (ac_dot_ab - ab_dot_ab <= 0)
+    mask_dir_opposite = (ab_dot_cd < 0) & (ac_dot_ab > 0) & (ac_dot_ab + ab_dot_cd - ab_dot_ab <= 0)
+    return np.any(colinear_mask & (mask_dir_same | mask_dir_opposite))
 
 
 if __name__ == '__main__':
@@ -33,15 +57,15 @@ if __name__ == '__main__':
     nt = 0
     nf = 0
     for i in range(10000):
-        a1 = np.random.randint(min_value, max_value, 2)
-        a2 = np.random.randint(min_value, max_value, 2)
-        b1 = np.random.randint(min_value, max_value, (200, 2))
-        b2 = np.random.randint(min_value, max_value, (200, 2))
-        res1 = intersect_segments_cross(a1, a2, b1, b2)
-        res2 = intersect_segments_explicit(a1, a2, b1, b2)
+        a = np.random.randint(min_value, max_value, 2)
+        b = np.random.randint(min_value, max_value, 2)
+        c = np.random.randint(min_value, max_value, (200, 2))
+        d = np.random.randint(min_value, max_value, (200, 2))
+        res1 = segments_intersect_cross(a, b, c, d)
+        res2 = segments_intersect_explicit(a, b, c, d)
         assert res1 == res2
     repeats = 10000
-    t_cross = timeit.Timer(lambda: intersect_segments_cross(a1, a2, b1, b2)).timeit(repeats)
-    t_explicit = timeit.Timer(lambda: intersect_segments_explicit(a1, a2, b1, b2)).timeit(repeats)
+    t_cross = timeit.Timer(lambda: segments_intersect_cross(a, b, c, d)).timeit(repeats)
+    t_explicit = timeit.Timer(lambda: segments_intersect_explicit(a, b, c, d)).timeit(repeats)
     print(f'cross version: {t_cross:.3f} seconds')
     print(f'explicit version: {t_explicit:.3f} seconds ({(t_explicit - t_cross) / t_cross * 100.0:+.2f}%)')
