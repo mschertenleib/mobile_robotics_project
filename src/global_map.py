c2e84ce4066b5e721e6d458a62dc51da9cdcfedd
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2023-11-19 10:00:29 +0100

    Merge pathfinding.py and map.py as global_map.py

--- src/global_map.py
+++ src/global_map.py
@@ -2,6 +2,14 @@ import cv2
 import numpy as np
 
 
+class Edge(object):
+    __slots__ = ['vertex', 'length']
+
+    def __init__(self, vertex, length):
+        self.vertex = vertex
+        self.length = length
+
+
 def image_info(img: np.ndarray):
     print(
         f'dtype: {img.dtype}, shape: {img.shape}, min: {img.min()}, max: {img.max()}')
@@ -23,10 +31,10 @@ def segments_intersect(a: np.ndarray, b: np.ndarray, c: np.ndarray, d: np.ndarra
     ab = b - a
     cd = d - c
     ac = c - a
-    cb_cd = np.cross(b - c, cd)
-    ca_cd = np.cross(a - c, cd)
-    ad_ab = np.cross(d - a, ab)
-    ac_ab = np.cross(ac, ab)
+    cb_cd = (b[0] - c[:, 0]) * cd[:, 1] - (b[1] - c[:, 1]) * cd[:, 0]
+    ca_cd = (a[0] - c[:, 0]) * cd[:, 1] - (a[1] - c[:, 1]) * cd[:, 0]
+    ad_ab = (d[:, 0] - a[0]) * ab[1] - (d[:, 1] - a[1]) * ab[0]
+    ac_ab = ac[:, 0] * ab[1] - ac[:, 1] * ab[0]
 
     # Detects intersections between non-colinear segments
     if np.any(
@@ -131,6 +139,57 @@ def draw_contour_orientations(img, contours, orientations):
         for i in range(n_points):
             brightness = i / (n_points - 1) * 255
             cv2.circle(img, contours[c][i][0], color=(brightness, brightness, brightness), radius=5, thickness=-1)
+
+
+def dijkstra(adjacency_list: list[list[Edge]], source: int, target: int):
+    """
+    source: the starting vertex of the search
+    target: the target vertex of the search
+    """
+    vertex_count = len(adjacency_list)
+    assert vertex_count > 0
+    assert 0 <= source < vertex_count
+    assert 0 <= target < vertex_count
+
+    dist = np.full(vertex_count, np.inf, dtype=float)
+    prev = np.full(vertex_count, -1, dtype=int)
+    visited = np.zeros(vertex_count, dtype=bool)
+    unvisited_count = vertex_count
+    dist[source] = 0.0
+
+    while unvisited_count > 0:
+        u = np.ma.MaskedArray(dist, visited).argmin()
+        if u == target:
+            break
+
+        visited[u] = True
+        unvisited_count -= 1
+
+        for edge in adjacency_list[u]:
+            if not visited[edge.vertex]:
+                alt = dist[u] + edge.length
+                if alt < dist[edge.vertex]:
+                    dist[edge.vertex] = alt
+                    prev[edge.vertex] = u
+
+    return reconstruct_path(prev, source, target)
+
+
+def reconstruct_path(prev, source: int, target: int) -> list[int]:
+    if target == source:
+        return [target]
+
+    if prev[target] < 0:
+        print('Target is not reachable from source')
+        return []
+
+    path = []
+    v = target
+    while v >= 0:
+        path.append(v)
+        v = prev[v]
+    path.reverse()
+    return path
 
 
 def main():
@@ -243,9 +302,24 @@ def floodfill_background():
     cv2.destroyAllWindows()
 
 
+def pathfinding_test():
+    graph = [[Edge(1, 7), Edge(2, 9), Edge(5, 14)],
+             [Edge(0, 7), Edge(2, 10), Edge(3, 15)],
+             [Edge(0, 9), Edge(1, 10), Edge(3, 11), Edge(5, 2)],
+             [Edge(1, 15), Edge(2, 11), Edge(4, 6)],
+             [Edge(3, 6), Edge(5, 9)],
+             [Edge(0, 14), Edge(2, 2), Edge(4, 9)]]
+    source = 0
+    target = 4
+    # Correct path for the above: [0, 2, 5, 4]
+    path = dijkstra(graph, source, target)
+    print(path)
+
+
 if __name__ == '__main__':
     main()
     # floodfill_background()
+    # pathfinding_test()
 
     """
     a1 = np.array([3.7, -1.9])
