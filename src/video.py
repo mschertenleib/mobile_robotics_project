7260ecfdfd3f63fb74d342574573885a914a4d6f
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2023-11-25 17:49:03 +0100

    Promising results for map detection using MORPH_OPEN

--- src/video.py
+++ src/video.py
@@ -9,7 +9,7 @@ def mouse_callback(event, x, y, flags, param):
         mouse_x, mouse_y = x, y
 
 
-def main():
+def detect_robot():
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
     # cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
     # cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
@@ -19,8 +19,8 @@ def main():
             print('Cannot read frame')
             break
 
-        # TODO: look into fastNlMeansDenoising
         img = cv2.GaussianBlur(frame, (15, 15), 0)
+        # img = cv2.medianBlur(frame, 15)
 
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         lower_red = np.array([60, 50, 50])
@@ -43,5 +43,29 @@ def main():
     cv2.destroyAllWindows()
 
 
+def detect_map():
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            print('Cannot read frame')
+            break
+
+        img = frame.copy()
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        _, img = cv2.threshold(img, 200, 1, cv2.THRESH_BINARY)
+
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        img = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel, iterations=2)
+
+        cv2.imshow('img', img * 255)
+        if cv2.waitKey(1) & 0xff == 27:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 if __name__ == '__main__':
-    main()
+    detect_map()
+    # detect_robot()
