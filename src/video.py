117610f9c64085abea0952791d4a0ab2a735fa21
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2023-11-25 22:14:05 +0100

    Bilateral filter for the win

--- src/video.py
+++ src/video.py
@@ -1,13 +1,5 @@
 from image_processing import *
 
-mouse_x, mouse_y = 0, 0
-
-
-def mouse_callback(event, x, y, flags, param):
-    global mouse_x, mouse_y
-    if event == cv2.EVENT_MOUSEMOVE:
-        mouse_x, mouse_y = x, y
-
 
 def detect_robot():
     cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
@@ -19,21 +11,19 @@ def detect_robot():
             print('Cannot read frame')
             break
 
-        img = cv2.GaussianBlur(frame, (15, 15), 0)
-        # img = cv2.medianBlur(frame, 15)
+        img = frame.copy()
+        img = cv2.bilateralFilter(img, 15, 150, 150)
 
         hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        lower_red = np.array([60, 50, 50])
-        upper_red = np.array([80, 255, 255])
+        lower_green = np.array([60, 50, 50])
+        upper_green = np.array([80, 255, 255])
+        mask = cv2.inRange(hsv, lower_green, upper_green)
 
-        global mouse_x, mouse_y
-        print(hsv[mouse_y, mouse_x])
-
-        mask = cv2.inRange(hsv, lower_red, upper_red)
+        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel, iterations=1)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel, iterations=1)
 
         cv2.imshow('color', img)
-        cv2.imshow('hue', hsv[:, :, 0])
-        cv2.setMouseCallback('hue', mouse_callback)
         cv2.imshow('mask', mask)
 
         if cv2.waitKey(1) & 0xff == 27:
@@ -67,5 +57,5 @@ def detect_map():
 
 
 if __name__ == '__main__':
-    detect_map()
-    # detect_robot()
+    # detect_map()
+    detect_robot()
