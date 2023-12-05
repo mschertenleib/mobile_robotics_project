5f8f177e7a3f291ceb4613355c055e6582282ad9
mathieu-schertenleib <schertenleib.mathieu@gmail.com>
2023-12-06 00:24:16 +0100

    Remove identity H from Kalman filter, explain motion model "hack"

--- src/kalman_filter.py
+++ src/kalman_filter.py
@@ -5,13 +5,17 @@ from parameters import *
 
 def kalman_filter(measurements: Optional[np.ndarray], mu_km: np.ndarray, sig_km: np.ndarray,
                   u_k: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
-    # TODO: remove H
-    # Global parameters
-    H = np.eye(3)
+    # Since our discretized motion model does not perfectly match reality, try to correct it by some extent.
+    # Since forward Euler integration tends to extrapolate "too far" for smooth trajectories, artificially reduce the
+    # contribution of the derivative to the state update. Note that this is just a "hack" that seems to work well enough
+    # for this system, and the specific multiplication factor was found through experimentation. For a more precise
+    # motion model, we should use a better integration scheme (Runge-Kutta 4 for example), and move the Kalman filter
+    # computation to the hardware, to get significant lower latencies on the inputs, as well as using a higher sampling
+    # frequency.
+    EFFECTIVE_SAMPLING_TIME = SAMPLING_TIME * 0.8
 
     # Prediction through the a priori estimate
-    # estimated mean of the state
-    EFFECTIVE_SAMPLING_TIME = SAMPLING_TIME * 0.8
+    # Estimated mean of the state
     mu_k_pred = np.array([[0.0], [0.0], [0.0]])
     mu_k_pred[0] = mu_km[0, 0] + (u_k[0] + u_k[1]) / 2 * EFFECTIVE_SAMPLING_TIME * -np.sin(
         mu_km[2, 0] - (u_k[0] - u_k[1]) / ROBOT_WHEEL_SPACING * EFFECTIVE_SAMPLING_TIME)
@@ -27,7 +31,7 @@ def kalman_filter(measurements: Optional[np.ndarray], mu_km: np.ndarray, sig_km:
         mu_km[2, 0] - (u_k[0] - u_k[1]) / ROBOT_WHEEL_SPACING * EFFECTIVE_SAMPLING_TIME)
 
     # Estimated covariance of the state
-    sig_k_pred = np.dot(G_k, np.dot(sig_km, G_k.T))
+    sig_k_pred = G_k @ sig_km @ G_k.T
     sig_k_pred += KALMAN_Q
 
     if measurements is not None:
@@ -38,15 +42,15 @@ def kalman_filter(measurements: Optional[np.ndarray], mu_km: np.ndarray, sig_km:
         y = mu_k_pred
 
     # Innovation / measurement residual
-    i = y - np.dot(H, mu_k_pred)
-    # measurement prediction covariance
-    S = np.dot(H, np.dot(sig_k_pred, H.T)) + KALMAN_R
+    i = y - mu_k_pred
+    # Measurement prediction covariance
+    S = sig_k_pred + KALMAN_R
 
     # Kalman gain (tells how much the predictions should be corrected based on the measurements)
-    K = np.dot(sig_k_pred, np.dot(H.T, np.linalg.inv(S)))
+    K = sig_k_pred @ np.linalg.inv(S)
 
     # A posteriori estimate
-    x_est = mu_k_pred + np.dot(K, i)
-    sig_est = sig_k_pred - np.dot(K, np.dot(H, sig_k_pred))
+    x_est = mu_k_pred + K @ i
+    sig_est = sig_k_pred - K @ sig_k_pred
 
     return x_est, sig_est
